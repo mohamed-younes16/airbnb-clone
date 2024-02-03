c6a8f75362d73b75e6dc9f2d6a305fbfb4febfcc
mohamed-younes16 <bettiouim77@gmail.com>
2024-02-03 10:38:01 +0100

    first commit

--- app/(root)/listing/[listingId]/page.tsx
+++ app/(root)/listing/[listingId]/page.tsx
@@ -6,6 +6,7 @@ import { DatePickerWithRange } from "@/components/inputs/DatePickerWithRange";
 import ListingShowCase from "@/components/ListingShowCase";
 import ImagesShow from "@/components/ImageShow";
 import { Separator } from "@/components/ui/separator";
+import { auth } from "@clerk/nextjs";
 
 const page = async ({
   params: { listingId },
@@ -14,7 +15,7 @@ const page = async ({
 }) => {
   const listing = await getListingById(listingId);
   const images = listing?.images;
-
+  const { userId } = auth();
   const fromDate =
     listing.reservations &&
     listing.reservations[0] &&
@@ -40,7 +41,7 @@ const page = async ({
               <ListingShowCase listing={listing} />
             </div>
 
-            {
+            {listing.ownerId !== userId && (
               <div className="flex-1 flex h-fit justify-center">
                 <DatePickerWithRange
                   fromDate={fromDate}
@@ -48,7 +49,7 @@ const page = async ({
                   price={listing.price || 0}
                 />
               </div>
-            }
+            )}
           </div>
         </>
       )}
